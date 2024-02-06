42a097012e87131d726192127907e2c29f2e536b
ST4R20RD <goncalo.estrelado@hotmail.com>
2024-02-06 23:01:49 +0000

    code along with @AneeshMistry

---
+++ SpringBoot Store/Store/Store/src/main/java/com/store/Store/repository/MySqlRepository.java
@@ -0,0 +1,8 @@
+package com.store.Store.repository;
+
+import com.store.Store.model.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MySqlRepository extends JpaRepository<Address, Integer>{
+
+}
